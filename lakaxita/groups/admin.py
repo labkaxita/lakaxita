b1fa15681acbcc308422d3b66b38a02f90a7104c
Unai Zalakain <contact@unaizalakain.info>
2012-11-29 00:50:00 +0100

    group admin as django's group inline

--- lakaxita/groups/admin.py
+++ lakaxita/groups/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group as AuthGroup
+from django.contrib.auth.admin import GroupAdmin
 
 from sorl.thumbnail.admin import AdminImageMixin
 
 from lakaxita.groups.models import Group
 
 
-class GroupAdmin(AdminImageMixin, admin.ModelAdmin):
+class GroupInline(admin.StackedInline):
+    model = Group
     search_fields = ('name', 'description')
-    fields = (('name', 'group'), 'image', 'description')
+    fields = ('name', 'image', 'description')
 
-admin.site.register(Group, GroupAdmin)
+class GroupAdmin(GroupAdmin):
+    inlines = [GroupInline]
+
+admin.site.unregister(AuthGroup)
+admin.site.register(AuthGroup, GroupAdmin)
