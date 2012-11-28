d359306dc999d51aba6667eebe6d0edb262761d7
Unai Zalakain <contact@unaizalakain.info>
2012-11-28 16:41:59 +0100

    render oembed template depending on the resource type

--- lakaxita/attachments/oembed_providers.py
+++ lakaxita/attachments/oembed_providers.py
@@ -5,12 +5,18 @@ from lakaxita.attachments.models import File
 
 
 class FileProvider(DjangoProvider):
+    provides = True
     resource_type = 'rich'
 
     class Meta:
         model = File
         named_view = 'attachments:file'
         fields_to_match = {'pk': 'pk'}
+        resource_template_names = {
+                'video': 'oembed/provider/video.yammy',
+                'audio': 'oembed/provider/audio.yammy',
+                'rich': 'oembed/provider/rich.yammy',
+                }
 
     def title(self, obj):
         return obj.name
@@ -18,6 +24,10 @@ class FileProvider(DjangoProvider):
     def request_resource(self, url, **kwargs):
         obj = self.get_object(url)
         self.resource_type = obj.type
+        self._meta.template_name = self._meta.resource_template_names.get(
+                self.resource_type,
+                self._meta.template_name,
+                )
         return super(FileProvider, self).request_resource(url, **kwargs)
 
 oembed.site.register(FileProvider)
