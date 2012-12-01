ff6a9ba41de84ef29b4ab91345cbaa746be25f02
Unai Zalakain <contact@unaizalakain.info>
2012-12-01 12:39:33 +0100

    lost_found.views.ItemDetail view test

--- lakaxita/lost_found/tests.py
+++ lakaxita/lost_found/tests.py
@@ -5,6 +5,7 @@ from django.test import Client
 from milkman.dairy import milkman
 
 from lakaxita.lost_found.models import Item, Notification
+from lakaxita.lost_found.forms import NotificationForm
 
 
 class ItemTestCase(unittest.TestCase):
@@ -88,3 +89,25 @@ class ItemListTestCase(unittest.TestCase):
     def tearDown(self):
         self.jacket.delete()
         self.trousers.delete()
+
+
+class ItemDetailTestCase(unittest.TestCase):
+    def setUp(self):
+        self.jacket = milkman.deliver(Item, name='jacket')
+        self.url = '/lost_found/jacket/'
+        self.client = Client()
+        self.response = self.client.get(self.url)
+
+    def test_status(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        self.assertEqual(self.response.template[0].name, 
+                'lost_found/item_detail.yammy')
+
+    def test_context(self):
+        self.assertEqual(self.response.context['item'], self.jacket)
+        self.assertEqual(type(self.response.context['form']), NotificationForm)
+
+    def tearDown(self):
+        self.jacket.delete()
