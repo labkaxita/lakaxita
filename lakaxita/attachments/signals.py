8326d1ac1ec438e5338a22073e1c9fcd7443b1c2
Unai Zalakain <contact@unaizalakain.info>
2012-12-11 19:32:01 +0100

    more robust attachment creation handling via signals

--- lakaxita/attachments/signals.py
+++ lakaxita/attachments/signals.py
@@ -16,14 +16,23 @@ signals.filebrowser_post_upload.connect(create_attachment)
 def rename_attachment_file(sender, **kwargs):
     old_path = kwargs['path']
     new_path = path.join(path.dirname(old_path), kwargs['new_name'])
-    attachment = File.objects.get(file=old_path)
-    attachment.file = new_path
-    attachment.save()
+    try:
+        attachment = File.objects.get(file=path)
+    except File.DoesNotExist:
+        attachment = File(file=new_path)
+    else:
+        attachment.file = new_path
+    finally:
+        attachment.save()
 signals.filebrowser_post_rename.connect(rename_attachment_file)
 
 
 def delete_attachment(sender, **kwargs):
     path = kwargs['path']
-    attachment = File.objects.get(file=path)
-    attachment.delete()
+    try:
+        attachment = File.objects.get(file=path)
+    except File.DoesNotExist:
+        pass
+    else:
+        attachment.delete()
 signals.filebrowser_pre_delete.connect(delete_attachment)
