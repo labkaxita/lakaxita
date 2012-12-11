82b44d3b9ff52bd11a2b6740dbf3eac09b357a08
Unai Zalakain <contact@unaizalakain.info>
2012-12-11 17:05:40 +0100

    add django-debug-toolbar

--- lakaxita/settings/development.py
+++ lakaxita/settings/development.py
@@ -15,3 +15,8 @@ DATABASES = {
         'PORT': '',                      
     }
 }
+
+
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INTERNAL_IPS = ('127.0.0.1',)
+INSTALLED_APPS += ('debug_toolbar',)
