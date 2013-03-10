44131b55cac2dadee7cb76b82bad455152a92139
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-10 18:35:39 +0100

    include \. in attachments file slugs

--- lakaxita/attachments/urls.py
+++ lakaxita/attachments/urls.py
@@ -5,5 +5,5 @@ from lakaxita.attachments.views import AttachmentDetail, FileRedirect
 
 urlpatterns = patterns('',
         url('^(?P<slug>(\w|\d|-)+)/$', AttachmentDetail.as_view(), name='detail'),
-        url('^file/(?P<slug>(\w|\d|-)+)/$', FileRedirect.as_view(), name='file'),
+        url('^file/(?P<slug>(\w|\d|-|\.)+)/$', FileRedirect.as_view(), name='file'),
 )
