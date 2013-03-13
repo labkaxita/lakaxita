d069098a41a5673797fede40897bf87b069c1f9e
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-13 20:40:37 +0100

    frontpage view

--- lakaxita/views.py
+++ lakaxita/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from lakaxita.news.models import News
+from lakaxita.gallery.models import Category
+
 
 class BaseView(TemplateView):
     template_name = 'home.yammy'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BaseView, self).get_context_data(*args, **kwargs)
+        context['news_list'] = News.objects.frontpage()
+        context['category_list'] = Category.objects.root_nodes()
+        return context
