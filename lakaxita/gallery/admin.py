6e3f4263222b51a6969eef1ff2d18f575aa537a9
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-13 20:57:52 +0100

    gallery admin

--- lakaxita/gallery/admin.py
+++ lakaxita/gallery/admin.py
@@ -4,11 +4,24 @@ from django.utils.translation import ugettext as _
 from grappelli_modeltranslation.admin import TranslationAdmin
 from mptt.admin import MPTTModelAdmin
 
-#from lakaxita.attachments.admin import AttachmentInline, FileInline
 from lakaxita.gallery.models import Category
 
 
 class CategoryAdmin(TranslationAdmin, MPTTModelAdmin):
+    filter_horizontal = ('attachments',)
     search_fields = ('name', 'description')
-
+    fieldsets = (
+            (None, {
+                'fields': (
+                    ('name', 'parent'),
+                    )}),
+            (_('Media'), {
+                'fields': (
+                    ('attachments',),
+                    )}),
+            (_('Meta'), {
+                'fields': (
+                    ('description',),
+                    )}),
+                )
 admin.site.register(Category, CategoryAdmin)
