ead8ffe025a50443193071b4f767315cf8a35556
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-20 18:42:20 +0100

    reverse func in every view for url reversing

--- lakaxita/static/js/lakaxita/app.js
+++ lakaxita/static/js/lakaxita/app.js
@@ -1,16 +1,16 @@
 define([
         'jquery', 
         'lakaxita/router', 
-        ], function (
-            $, 
-            Router
-            ) { 
-
+        ], function ($, Router) { 
 
     function App() {
         this.boot = function(container) {
             container = $(container);
-            this.router = new Router({el: container});
+            router = new Router({el: container});
+            this.router = router;
+            Backbone.View.prototype.reverse = function(route, subroute, model) { 
+                return router.reverse(route, subroute, model);
+            };
             Backbone.history.start();
         };
         this.Router = Router;
