e640471cd7a1357fc08fd398cd660fcd00eb822f
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-20 20:51:38 +0100

    pass right args to Backbone.Subroute constructor

--- lakaxita/static/js/lakaxita/utils/routers.js
+++ lakaxita/static/js/lakaxita/utils/routers.js
@@ -1,9 +1,9 @@
 define(['backbone.subroute', 'backbone', 'underscore'], function(subroute, Backbone, _) {
 
     SubRouter = Backbone.SubRoute.extend({
-        constructor: function(options) {
+        constructor: function(prefix, options) {
             this._routes = _.clone(this.routes);
-            Backbone.SubRoute.prototype.constructor.call(this, options);
+            Backbone.SubRoute.prototype.constructor.call(this, prefix, options);
         },
         initialize: function(options) {
             this.el = options.el;
