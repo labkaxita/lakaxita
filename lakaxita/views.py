c2c221f1791c0c2638968fe4591df9646cdc70c2
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-24 18:36:29 +0100

    raise 404 if template path passed to JSTemplateView does not exist

--- lakaxita/views.py
+++ lakaxita/views.py
@@ -1,6 +1,8 @@
 from os import path
 
 from django.views.generic import TemplateView
+from django.template.base import TemplateDoesNotExist
+from django.http import Http404
 
 from lakaxita.news.models import News
 from lakaxita.gallery.models import Category
@@ -21,3 +23,13 @@ class JSTemplateView(TemplateView):
         p = path.join('js', self.kwargs['path'])
         return [ '{path}.{ext}'.format(path=p, ext=ext) for ext in\
                 ('yammy', 'html') ]
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super(JSTemplateView, self).render_to_response(
+                context, **response_kwargs)
+        try:
+            response.resolve_template(response.template_name)
+        except TemplateDoesNotExist:
+            raise Http404
+        else:
+            return response
