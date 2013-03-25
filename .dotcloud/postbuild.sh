cab46041c31321d3d0a510e091018c0a94319591
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-25 16:12:42 +0100

    impossible to run buildout in postinstall, dotcloud timeout reached

--- .dotcloud/postbuild.sh
+++ .dotcloud/postbuild.sh
@@ -2,3 +2,4 @@ cp ./.dotcloud/postinstall ./postinstall
 cp ./.dotcloud/nginx.conf ./nginx.conf
 mkdir -p /home/dotcloud/data/media /home/dotcloud/volatile/static
 python bootstrap.py
+./bin/buildout -c production.cfg -N
