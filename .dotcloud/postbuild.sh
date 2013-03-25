bc6c9513ef0767a879979d731c11739183cdef1c
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-25 15:49:18 +0100

    move buildout to postinstall hook

--- .dotcloud/postbuild.sh
+++ .dotcloud/postbuild.sh
@@ -1,6 +1,4 @@
 cp ./.dotcloud/postinstall ./postinstall
 cp ./.dotcloud/nginx.conf ./nginx.conf
-python bootstrap.py
-./bin/buildout -c production.cfg -N
-cp ./bin/django.wsgi ./wsgi.py
 mkdir -p /home/dotcloud/data/media /home/dotcloud/volatile/static
+python bootstrap.py
