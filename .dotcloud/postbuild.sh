3253b2573377c5574b112fa8783def1355d42d8b
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-25 16:24:08 +0100

    copy wsgi file before installation finishes

--- .dotcloud/postbuild.sh
+++ .dotcloud/postbuild.sh
@@ -3,3 +3,4 @@ cp ./.dotcloud/nginx.conf ./nginx.conf
 mkdir -p /home/dotcloud/data/media /home/dotcloud/volatile/static
 python bootstrap.py
 ./bin/buildout -c production.cfg -N
+cp ./bin/django.wsgi ./wsgi.py
