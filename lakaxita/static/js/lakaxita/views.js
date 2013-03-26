572e58b9a5a06eb5b235e60e06fcf301098117f0
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-27 00:45:02 +0100

    change language setting djangolanguage cookie and rerendering template

--- lakaxita/static/js/lakaxita/views.js
+++ lakaxita/static/js/lakaxita/views.js
@@ -5,6 +5,16 @@ define([
     Nav = Views.View.extend({
         tagName: 'body',
         template: 'nav',
+        events: {
+            'click ul#language > li > a': 'language',
+        },
+        language: function(event) {
+            event.preventDefault();
+            var lang = this.$(event.target).text();
+            document.cookie = 'django_language='+lang;
+            require.undef(this.template_uri());
+            this.render();
+        },
     });
 
     return {
