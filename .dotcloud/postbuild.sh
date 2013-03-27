10e89c13362c16cd9e1586722057de97162e643d
Unai Zalakain <unai@gisa-elkartea.org>
2013-03-27 10:53:04 +0100

    custom dir for buildout directives

--- .dotcloud/postbuild.sh
+++ .dotcloud/postbuild.sh
@@ -2,5 +2,5 @@ cp ./.dotcloud/postinstall ./postinstall
 cp ./.dotcloud/nginx.conf ./nginx.conf
 mkdir -p /home/dotcloud/data/media /home/dotcloud/volatile/static
 python bootstrap.py
-./bin/buildout -c production.cfg -N
+./bin/buildout -c buildout.d/production.cfg -N
 cp ./bin/django.wsgi ./wsgi.py
