d7353d523375405fc8cc13c491b6848431ae52ae
Unai Zalakain <unai@gisa-elkartea.org>
2013-04-02 23:05:59 +0200

    use base template instead of home template

--- lakaxita/views.py
+++ lakaxita/views.py
@@ -4,18 +4,9 @@ from django.views.generic import TemplateView
 from django.template.base import TemplateDoesNotExist
 from django.http import Http404
 
-from lakaxita.news.models import News
-from lakaxita.gallery.models import Category
-
 
 class BaseView(TemplateView):
-    template_name = 'home.yammy'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(BaseView, self).get_context_data(*args, **kwargs)
-        context['news_list'] = News.objects.frontpage()
-        context['category_list'] = Category.objects.root_nodes()
-        return context
+    template_name = 'base.yammy'
 
 
 class JSTemplateView(TemplateView):
