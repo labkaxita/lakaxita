bba8c77219076e8204691f8f4dc9bf2937d6e946
Unai Zalakain <unai@gisa-elkartea.org>
2013-04-05 21:59:46 +0200

    create divs for left/right arrows and tie them to kinetic movements

--- lakaxita/static/js/lakaxita/utils/views.js
+++ lakaxita/static/js/lakaxita/utils/views.js
@@ -28,6 +28,8 @@ define([
                 this.$list = zen(this.list);
             };
             this.$el.html(this.$list);
+            this.$list.before(zen('div#left'));
+            this.$list.after(zen('div#right'));
             this.collection.on('sync', this.render, this);
         },
         render: function() {
@@ -42,9 +44,17 @@ define([
         },
         events: {
             'click li > a': 'empty',
+            'click #left': 'scrollLeft',
+            'click #right': 'scrollRight',
         },
         empty: function() {
             this.$el.empty();
+        },
+        scrollLeft: function() {
+            this.$el.kinetic('start', {velocity: -10, decelerate: true});
+        },
+        scrollRight: function() {
+            this.$el.kinetic('start', {velocity: 10, decelerate: true});
         },
     });
 
