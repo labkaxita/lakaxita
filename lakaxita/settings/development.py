cdf0a010363034e5fd7b659fdb4b9edf5dcc639b
Unai Zalakain <unai@gisa-elkartea.org>
2013-04-12 01:54:28 +0200

    don't use django-debug-toolbar
    it doesn't make any sense in a totally client-side web

--- lakaxita/settings/development.py
+++ lakaxita/settings/development.py
@@ -18,7 +18,6 @@ DATABASES = {
 }
 
 
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INTERNAL_IPS = ('127.0.0.1',)
-INSTALLED_APPS += ('debug_toolbar', 'django_nose')
+INSTALLED_APPS += ('django_nose',)
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
