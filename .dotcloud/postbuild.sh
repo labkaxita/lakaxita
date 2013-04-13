22edb80c0e9c643b77aab2b8d1b75f23611f15d8
Unai Zalakain <unai@gisa-elkartea.org>
2013-04-13 18:11:42 +0200

    run buildout at dotcloud in non-newest mode

--- .dotcloud/postbuild.sh
+++ .dotcloud/postbuild.sh
@@ -3,5 +3,5 @@ cp ./.dotcloud/nginx.conf ./nginx.conf
 mkdir -p /home/dotcloud/data/media /home/dotcloud/volatile/static
 sed -i 's/development/production/g' buildout.cfg
 python bootstrap.py
-./bin/buildout
+./bin/buildout -N
 cp ./bin/django.wsgi ./wsgi.py
